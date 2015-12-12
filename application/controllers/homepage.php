d34600c23434039e7ca95163aa08390846237330
Rohin Mittal <rohinmittal@Rohins-MacBook-Pro.local>
2015-12-12 17:49:44 -0500

    removing sidebar from homepage

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -4,8 +4,8 @@ class Homepage extends CI_Controller {
 	public function index() {
 		$this->load->helper('form');
 		$this->load->view('header');
-		$data=array('resultset' => array(), 'languages' => array(), 'facets' => array(), 'httpLink' => '');
-		$this->load->view('results', $data);
+		//$data=array('resultset' => array(), 'languages' => array(), 'facets' => array(), 'httpLink' => '');
+		//$this->load->view('results', $data);
 		$this->load->view('footer');
 	}
 }
